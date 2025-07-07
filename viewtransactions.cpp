e2c85e62a7587da0c6c7f316340e5fa6c29682d6
aasishhumagain <aasishhumagain1111@gmail.com>
2025-07-07 21:01:04 +0545

    Working transactions page remarks

--- viewtransactions.cpp
+++ viewtransactions.cpp
@@ -1,8 +1,10 @@
 #include "viewtransactions.h"
 #include "ui_viewtransactions.h"
+
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QTableWidgetItem>
+#include <QHeaderView>
 #include <QDebug>
 
 viewtransactions::viewtransactions(QWidget *parent, int userId) :
@@ -27,10 +29,15 @@ void viewtransactions::onBackButtonClicked()
 
 void viewtransactions::loadTransactionData()
 {
+    // Setup table columns
     ui->tableWidgetTransactions->setRowCount(0);
     ui->tableWidgetTransactions->setColumnCount(5);
+
     QStringList headers = {"Date", "Category", "Type", "Amount", "Remarks"};
     ui->tableWidgetTransactions->setHorizontalHeaderLabels(headers);
+
+    // Stretch Remarks column for better readability
+    ui->tableWidgetTransactions->horizontalHeader()->setSectionResizeMode(4, QHeaderView::Stretch);
 
     QString sql = R"(
         SELECT date, category, type, amount, remarks
@@ -44,7 +51,22 @@ void viewtransactions::loadTransactionData()
             'Monthly Goal' AS category,
             'MonthlyGoal' AS type,
             amount,
-            '' AS remarks
+            'Monthly Goal for ' ||
+            CASE CAST(month AS INTEGER)
+                WHEN 1 THEN 'January'
+                WHEN 2 THEN 'February'
+                WHEN 3 THEN 'March'
+                WHEN 4 THEN 'April'
+                WHEN 5 THEN 'May'
+                WHEN 6 THEN 'June'
+                WHEN 7 THEN 'July'
+                WHEN 8 THEN 'August'
+                WHEN 9 THEN 'September'
+                WHEN 10 THEN 'October'
+                WHEN 11 THEN 'November'
+                WHEN 12 THEN 'December'
+                ELSE 'Unknown'
+            END || ' ' || year AS remarks
         FROM monthly_goals
         WHERE user_id = :uid2
 
@@ -65,10 +87,8 @@ void viewtransactions::loadTransactionData()
     while (query.next()) {
         ui->tableWidgetTransactions->insertRow(row);
         for (int col = 0; col < 5; ++col) {
-            ui->tableWidgetTransactions->setItem(
-                row, col,
-                new QTableWidgetItem(query.value(col).toString())
-                );
+            QString value = query.value(col).toString();
+            ui->tableWidgetTransactions->setItem(row, col, new QTableWidgetItem(value));
         }
         row++;
     }
