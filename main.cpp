ce8628d8a1c695755bd905efd40e2537a2d25916
shreyapaudyal <paudyalshreya12345@gmail.com>
2025-08-02 18:00:12 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 #include "databasemanager.h"
 #include "login.h"
 
+
 int main(int argc, char *argv[])
+
 {
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+
+
     QApplication a(argc, argv);
 
     if (!DatabaseManager::instance().openDatabase()) {
